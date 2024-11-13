28c79fb6d0fe38c136dd423828afb5ce0852913d
Tommy H. Yeargin, Jr <88904457+yeargin2021@users.noreply.github.com>
2024-11-13 09:21:01 -0500

    commit

---
+++ cmake-build-debug/number_two.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Tommy H Yeargin Jr on 11/13/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int two = 2;
+string t = "Tommy";
+string r = "Rene";
+
+void intro() {
+    cout << "We are " + t + " and " + r + "!" << endl;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "my_functions.h"
+
+int main() {
+    std::cout << two + two << std::endl;
+    intro();
+    return 0;
+}
